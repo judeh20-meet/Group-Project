929b5c8d153cd8f332c50899b14e9bc56bcec3f3
myname21-meet <myname21@meet.mit.edu>
2019-08-05 06:20:14 -0400

    added

---
+++ app.py
@@ -0,0 +1,17 @@
+from databases import *
+from flask import Flask, request, redirect, render_template
+from flask import session as login_session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ databases.py
@@ -0,0 +1,18 @@
+from model import Base, User
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///users.db?check_same_thread=False')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def add_user(name,secret_word):
+    """Add a user to the DB."""
+    user = User(username=name)
+    #there is a line of code missing here, what else does a user need?
+    user.hash_password(secret_word)
+    session.add(user)
+    session.commit()
+
